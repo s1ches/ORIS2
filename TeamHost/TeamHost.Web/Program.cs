b527bf5f2fb3c89ead4958a41f582c595fdab829
1chess <1chessmic@gmail.com>
2024-03-29 13:59:58 +0300

    created streaming service

--- TeamHost/TeamHost.Web/Program.cs
+++ TeamHost/TeamHost.Web/Program.cs
@@ -1,4 +1,18 @@
+using System.Net;
+using LiveStreamingServerNet;
+using LiveStreamingServerNet.Flv.Installer;
+using LiveStreamingServerNet.Networking.Helpers;
+
+await using var liveStreamingServer = LiveStreamingServerBuilder.Create()
+    .ConfigureRtmpServer(options => options.AddFlv())
+    .ConfigureLogging(options => options.AddConsole())
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBackgroundServer(liveStreamingServer, new IPEndPoint(IPAddress.Any, 1935));
+builder.Services.AddHttpClient();
+builder.Services.AddLogging();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +26,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseWebSockets();
+app.UseWebSocketFlv(liveStreamingServer);
+
+app.UseHttpFlv(liveStreamingServer);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
