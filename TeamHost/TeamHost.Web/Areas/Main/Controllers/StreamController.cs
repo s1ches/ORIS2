b96bafa068526e357df76660fa76cae2cb153518
1chess <1chessmic@gmail.com>
2024-03-29 14:09:37 +0300

    clean StreamController

--- TeamHost/TeamHost.Web/Areas/Main/Controllers/StreamController.cs
+++ TeamHost/TeamHost.Web/Areas/Main/Controllers/StreamController.cs
@@ -1,4 +1,3 @@
-using System.Buffers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TeamHost.Web.Areas.Main.Controllers;
@@ -9,17 +8,4 @@ public class StreamController(HttpClient client) : Controller
     public IActionResult Streams() => View();
 
     public IActionResult StreamPage() => View();
-    
-    public async Task GetStream(CancellationToken cancellationToken)
-    {
-        var message = new HttpRequestMessage();
-        message.RequestUri = new Uri("https://localhost:44318/live/demo.flv");
-        message.Method = HttpMethod.Get;
-
-        var response = await client.SendAsync(message, cancellationToken);
-
-        await Response.BodyWriter.WriteAsync(await response.Content.ReadAsByteArrayAsync(cancellationToken),
-            cancellationToken);
-        await Response.Body.FlushAsync(cancellationToken);
-    }
 }
